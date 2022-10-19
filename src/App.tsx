41a5519f267a72003a2fafee263111a63e7d231f
Alexy Viegas <alexyviegas@gmail.com>
2022-10-19 00:29:56 -0300

    more styling

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,24 @@
-import { useState, useEffect } from 'react';
+import { useState } from 'react';
+import ReactMarkdown from 'react-markdown';
+import './App.css';
 
 function App() {
   const [input, setInput] = useState('');
 
   return (
-    <div className="flex flex-col bg-slate-100 h-screen">
+    <div className="app">
       {/* header */}
-      <div className="bg-slate-900 text-gray-100 text-center py-2 text-2xl">
+      <div className="header">
         <h3>Markdown Live Preview</h3>
       </div>
       {/* content */}
-      <div className="flex flex-row gap-1 h-full">
+      <div className="box">
         {/* input */}
-        <div className="flex flex-col h-full w-1/2 px-2 py-3 rounded">
-          <h4 className="font-bold bg-slate-500 text-gray-100 py-2 text-center rounded-t ">
-            Input
-          </h4>
+        <div className="item">
+          <h4>Input</h4>
           <textarea
-            className="w-full resize-none border border-slate-500 border-solid rounded-b h-full 
+            autoFocus
+            className=" 
               "
             onChange={(e) => setInput(e.target.value)}
             cols={60}
@@ -25,13 +26,9 @@ function App() {
           />
         </div>
         {/* markdown */}
-        <div className="flex flex-col h-full w-1/2 px-2 py-3 rounded">
-          <h4 className="font-bold bg-slate-500 text-gray-100 py-2 text-center rounded-t">
-            Markdown Output
-          </h4>
-          <p className="w-full h-full resize-none border border-slate-500 border-solid rounded-b bg-slate-50">
-            {input}
-          </p>
+        <div className="item">
+          <h4 className="">Markdown Output</h4>
+          <ReactMarkdown className="markdown">{input}</ReactMarkdown>
         </div>
       </div>
     </div>
